708ec65f17d9243c000b8680b23488ecf4056c8a
Anton Nikolov <antonio85pd@gmail.com>
2018-11-22 00:30:20 +0200

    implementing Node and SinglyLinkedList with all it's methods

---
+++ LinkedList/SinglyLinkedList.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class SinglyLinkedList<T> : ICollection<T>
+    {
+        public LinkedListNode<T> Head { get; private set; }
+
+        public LinkedListNode<T> Tail { get; private set; }
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void Add(T item)
+        {
+            LinkedListNode<T> node = new LinkedListNode<T>(item);
+            AddLast(node);
+        }
+
+        public void AddFirst(LinkedListNode<T> node)
+        {
+            LinkedListNode<T> temp = Head;
+            Head = node;
+            Head.Next = temp;
+
+            if (Count == 1)
+            {
+                Tail = node;
+            }
+
+            Count++;
+        }
+
+        private void AddLast(LinkedListNode<T> node)
+        {
+            if (Count == 0)
+            {
+                Head = node;
+            }
+            else
+            {
+                Tail.Next = node;
+            }
+
+            Tail = node;
+            Count++;
+        }
+
+        public void Clear()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            LinkedListNode<T> current = Head;
+
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            LinkedListNode<T> current = Head;
+
+            while (current != null)
+            {
+                array[arrayIndex++] = current.Value;
+                current = current.Next;
+            }
+        }
+
+
+        public bool Remove(T item)
+        {
+            LinkedListNode<T> previous = null;
+            LinkedListNode<T> current = Head;
+
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+
+                        if (previous.Next == null)
+                        {
+                            Tail = previous;
+                        }
+                    }
+                    else
+                    {
+                        RemoveFirst();
+                    }
+
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public void RemoveFirst()
+        {
+            if (Count != 0)
+            {
+                Head = Head.Next;
+
+                Count--;
+                if (Count == 0)
+                {
+                    Tail = null;
+                }
+            }
+            
+        }
+
+        public void RemoveLast()
+        {
+            if (Count != 0)
+            {
+                if (Count == 1)
+                {
+                    Head = null;
+                    Tail = null;
+                }
+                else
+                {
+                    LinkedListNode<T> current = Head;
+
+                    while (current != Tail)
+                    {
+                        current = current.Next;
+                    }
+
+                    current.Next = null;
+                    Tail = current;
+                }
+
+                Count--;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            LinkedListNode<T> current = Head;
+
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
